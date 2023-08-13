9ba8744266e952613ce80a017371d2ffb0ff50da
lunarifish <messenger@lunariio.work>
2023-08-13 20:32:55 +0800

    simplify code

--- main.py
+++ main.py
@@ -1,116 +1,73 @@
-from textsaveload import *
-from pic_scale import picscale
-from locate import locate
+
+import sys
 import os
 import cv2
 import mido
+import numpy as np
 
 
-# scaling picture
-
-(loc_pic_orig, loc_pic_scaled) = locate()       # define the location of the picture
-infile = loc_pic_orig
-outfile = loc_pic_scaled
-height = input("set picture height(1~127):")    # input picture height
-stat_pic = picscale(loc_pic_orig, loc_pic_scaled, height)
 
 
-# main
+## Utility Functions ##
+
+def rotate_clockwise(matrix):
+    transposed_matrix = list(zip(matrix))                                   # transpose the matrix
+    return np.array(list(map(list, reversed(transposed_matrix))))           # reverse the transposed matrix
+
+def data_standardization(data_max, data_min, current_data):
+    return (current_data - data_min) / (data_max - data_min)
+
+###########################
+
+
+
+
+args = sys.argv[1:]
+PICTURE_PATH = r"123.png"
+OUTPUT_PATH = ...
+MIDI_PIC_HEIGHT = 127
+
+
+
+
+## Scaling picture ##
+try:
+    img = cv2.imread(PICTURE_PATH)
+except Exception:
+    os._exit(1)
+
+x_original, y_original = img.shape[1::-1]
+resize_multiplier = MIDI_PIC_HEIGHT / y_original
+
+img_scaled = cv2.resize(img, (round(x_original * resize_multiplier), MIDI_PIC_HEIGHT))
+x_scaled, y_scaled = round(x_original * resize_multiplier), MIDI_PIC_HEIGHT
+
+img_scaled_grayscale = cv2.cvtColor(img_scaled, cv2.COLOR_BGR2GRAY)
+
+img_flattened = img_scaled_grayscale.flatten()
+min_gray, max_gray = min(img_flattened), max(img_flattened)
+
+img_scaled_grayscale_rotated = np.array(list(reversed(np.rot90(img_scaled_grayscale))))
+
+######################
+
+
 
 mid = mido.MidiFile()
 track = mido.MidiTrack()
 mid.tracks.append(track)
 
-img = cv2.imread(loc_pic_scaled)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+for line_index, line in enumerate(img_scaled_grayscale_rotated):
+    for index, pixel in enumerate(line):
+        velocity = int(data_standardization(max_gray, min_gray, pixel) * 127)
+        track.append(mido.Message("note_on", note=MIDI_PIC_HEIGHT - index, velocity=velocity, time=0))
+    for index, pixel in enumerate(line):
+        velocity = int(data_standardization(max_gray, min_gray, pixel) * 127)
+        if index == 0:
+            track.append(mido.Message("note_off", note=MIDI_PIC_HEIGHT - index, velocity=velocity, time=200))
+        else:
+            track.append(mido.Message("note_off", note=MIDI_PIC_HEIGHT - index, velocity=velocity, time=0))
 
-
-
-# initialize counters
-scan_x = 1                                         # colomns
-scan_y = 1                                         # rows
-pixel_count = 0                                    # scaned pixels
-x = stat_pic[0]                                    # original picture width
-y = stat_pic[1]                                    # original picture height
-x_scale = stat_pic[2]                              # scaled picture width
-y_scale = stat_pic[3]                              # scaled picture height
-###
-
-# find extreme values
-while scan_x <= x_scale - 1:
-    scan_y = 1               # reset rows
-
-    while scan_y <= y_scale - 1:
-
-        #counting
-        pixel_count = pixel_count + 1
-        print("scanning", pixel_count, "pixels")
-        gray_current = gray[scan_y, scan_x]
-        gray_variable = [gray_current]                     # save grayscale value of all pixels
-        text_save(gray_variable,"grayscale_value.txt")     ##
-        ###
-        scan_y = scan_y + 1    # rows + 1
-    scan_x = scan_x + 1 
-
-gray_all_str = text_read("grayscale_value.txt")
-gray_all = [int(x) for x in gray_all_str]
-gray_min = min(gray_all)
-gray_max = max(gray_all)
-print(gray_max, gray_min)
-
-
-# output midi file
-scan_x = 1                                         # colomns
-scan_y = 1                                         # rows
-pixel_count = 0                                    # scaned pixels
-###
-
-while scan_x <= x_scale - 1:
-    scan_y = 1               # reset rows
-    gray_current = gray[scan_y, scan_x]
-    gray_calculated = (gray_current - gray_min) / (gray_max - gray_min)       # greyscale values dispersion standardization
-    velocity = int(gray_calculated * 127)              # velocity
-    note = int(127 - scan_y)                           # pitch
-    time = 0                                           # time
-    track.append(mido.Message("note_on", note=note, velocity=velocity, time=0))          # NOTE_ON
-
-    while scan_y <= y_scale - 1:
-
-        # counting
-        pixel_count = pixel_count + 1
-        print("outputing", pixel_count, "notes")
-        gray_current = gray[scan_y, scan_x]
-        gray_calculated = (gray_current - gray_min) / (gray_max - gray_min)       # greyscale values dispersion standardization
-        velocity = int(gray_calculated * 127)              # velocity
-        note = int(127 - scan_y)                           # pitch
-        time = 0                                           # time
-        ###
-
-        track.append(mido.Message("note_on", note=note, velocity=velocity, time=0))        # NOTE_ON
-        scan_y = scan_y + 1    # rows + 1
-    scan_y = 1                 # reset rows
-    gray_current = gray[scan_y, scan_x]
-    gray_calculated = (gray_current - gray_min) / (gray_max - gray_min)       # greyscale values dispersion standardization
-    velocity = int(gray_calculated * 127)              # velocity
-    note = int(127 - scan_y)                           # pitch
-    time = 0                                           # time
-    track.append(mido.Message("note_off", note=note, velocity=velocity, time=480))        # NOTE_OFF
-
-    while scan_y<= y_scale - 1:
-        # counting
-        pixel_count = pixel_count + 1
-        gray_current = gray[scan_y, scan_x]
-        gray_calculated = (gray_current - gray_min) / (gray_max - gray_min)       # greyscale values dispersion standardization
-        velocity = int(gray_calculated * 127)              # velocity
-        note = int(127 - scan_y)                           # pitch
-        time = 0                                           # time
-        ###
-
-        track.append(mido.Message("note_off", note=note, velocity=velocity, time=0))        # NOTE_OFF
-        scan_y = scan_y + 1    # rows + 1
-    scan_x = scan_x + 1
 
 mid.save("output.mid")
-print("output.mid\n")
-os.system("pause >> nul")
