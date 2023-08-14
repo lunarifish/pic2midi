98504f334a4fe5637617008932f356a4ae9ffe7f
lunarifish <messenger@lunariio.work>
2023-08-14 19:29:28 +0800

    improve efficiency

--- main.py
+++ main.py
@@ -6,23 +6,30 @@ import mido
 import numpy as np
 
 
+
 def data_standardization(data_max, data_min, current_data):
     return (current_data - data_min) / (data_max - data_min)
 
 
 
-
+## Process user input
 args = sys.argv[1:]
-if len(args) != 2:
-    print("Usage: main.py picture_path midi_height")
+try:
+    if len(args) != 2:
+        print("Usage: main.py picture_path midi_height")
+        os._exit(1)
+    if int(args[1]) not in range(1, 128):
+        print("MIDI pic height too big/small(range 1~127)")
+
+    PICTURE_PATH, MIDI_PIC_HEIGHT = args[0], int(args[1])
+except Exception:
     os._exit(1)
 
-PICTURE_PATH, MIDI_PIC_HEIGHT = args[0], int(args[1])
+######################
 
 
 
-
-## Scaling picture ##
+## Scaling picture
 try:
     img = cv2.imread(PICTURE_PATH)
 except Exception:
@@ -39,11 +46,23 @@ img_scaled_grayscale = cv2.cvtColor(img_scaled, cv2.COLOR_BGR2GRAY)
 img_flattened = img_scaled_grayscale.flatten()
 min_gray, max_gray = min(img_flattened), max(img_flattened)
 
-img_scaled_grayscale_rotated = np.array(list(reversed(np.rot90(img_scaled_grayscale))))
+img_scaled_grayscale_rotated = np.rot90(img_scaled_grayscale)
 
 ######################
 
 
+
+## Pre-compute grey value to velocity table
+velocity_gray_table = [None] * min_gray
+for i in range(min_gray, max_gray + 1):
+    velocity_gray_table.append(int(data_standardization(max_gray, min_gray, i) * 127))
+
+######################
+
+
+
+
+## Generate .mid file
 
 mid = mido.MidiFile()
 track = mido.MidiTrack()
@@ -52,14 +71,12 @@ mid.tracks.append(track)
 
 for line in img_scaled_grayscale_rotated:
     for index, pixel in enumerate(line):
-        velocity = int(data_standardization(max_gray, min_gray, pixel) * 127)
-        track.append(mido.Message("note_on", note=MIDI_PIC_HEIGHT - index, velocity=velocity, time=0))
+        track.append(mido.Message("note_on", note=index, velocity=velocity_gray_table[pixel], time=0))
     for index, pixel in enumerate(line):
-        velocity = int(data_standardization(max_gray, min_gray, pixel) * 127)
         if index == 0:
-            track.append(mido.Message("note_off", note=MIDI_PIC_HEIGHT - index, velocity=velocity, time=200))
+            track.append(mido.Message("note_off", note=index, velocity=velocity_gray_table[pixel], time=200))
         else:
-            track.append(mido.Message("note_off", note=MIDI_PIC_HEIGHT - index, velocity=velocity, time=0))
+            track.append(mido.Message("note_off", note=index, velocity=velocity_gray_table[pixel], time=0))
 
 
 output_filename = "".join(PICTURE_PATH.split(".")[:-1]) + ".mid"
